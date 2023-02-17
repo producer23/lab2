26230bfe953bfba4b46916bb250d49a45099d0b3
producer23 <cherep633@gmail.com>
2023-02-17 11:24:43 +0300

    master 1

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        if (args.length > 0 && !args[0].isEmpty()) {
+            System.out.println("Hello, " + args[0]);
+        }
+    }
+}
