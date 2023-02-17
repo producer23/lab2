28f1de25ab4871b9f5f09da5c85cdd775ee37063
producer23 <cherep633@gmail.com>
2023-02-17 11:28:03 +0300

    master 2

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static void func(double x, double y) {
+        double res = (3.0 * Math.pow(Math.cos(x - Math.PI / 6.0), 2)) / (0.5 + Math.sin(y * y));
+        System.out.println("Результат = " + res);
+    }
+
     public static void main(String[] args) {
         if (args.length > 0 && !args[0].isEmpty()) {
             System.out.println("Hello, " + args[0]);
         }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите X и Y:");
+        func(sc.nextDouble(), sc.nextDouble());
     }
 }
